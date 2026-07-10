fb14c4c11cd055fa4145a8c40d262b96f137f5c2
duran-ni <duran_ni_7@hotmail.com>
2026-07-10 10:37:38 +0200

    feat: extend FizzBuzz with digit detection stage 2

--- fizzbuzz/src/main/java/com/kata/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/kata/FizzBuzz.java
@@ -3,9 +3,17 @@ package com.kata;
 public class FizzBuzz {
 
     public String evaluate(int number) {
-        if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
+        boolean divBy3 = number % 3 == 0;
+        boolean divBy5 = number % 5 == 0;
+        boolean has3   = String.valueOf(number).contains("3");
+        boolean has5   = String.valueOf(number).contains("5");
+
+        boolean isFizz = divBy3 || has3;
+        boolean isBuzz = divBy5 || has5;
+
+        if (isFizz && isBuzz) return "FizzBuzz";
+        if (isFizz)           return "Fizz";
+        if (isBuzz)           return "Buzz";
         return String.valueOf(number);
     }
 }
